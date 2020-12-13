10bc41470d774855d4b13a6795671f7dedd0e963
Gergely Nemeth <mail@nemethgergely.com>
2020-12-13 11:00:35 -0800

    chore(project): add baseui to test overrides with

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+
+const engine = new Styletron();
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+export const decorators = [
+  (Story) => (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Story />
+      </BaseProvider>
+    </StyletronProvider>
+  ),
+];
