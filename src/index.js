9d80670e8cd00895741f3496e0ba269c1bbb7af7
Gergely Nemeth <mail@nemethgergely.com>
2020-12-13 09:01:01 -0800

    chore(project): add storybook

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Codelabs () {
+  return <span>hello</span>
+}
+
+export {Codelabs}
---
+++ stories/Codelabs.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Codelabs } from '../src/index.js';
+
+export default {
+  title: 'Codelabs/Example',
+  component: Codelabs,
+};
+
+const Template = (args) => <Codelabs {...args} />;
+
+export const FirstStory = Template.bind({});
